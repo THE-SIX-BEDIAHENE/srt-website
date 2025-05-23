cffe938299e011ef0abec7ab645366ebc7cdfca4
Sarah0360 <amediorsarah@gmail.com>
2025-05-23 12:14:09 +0000

    Fix: correct Path in App.jsx import for Vercel build

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Homepage from "./pages/Homepage";
-import AboutPage from "./pages/Aboutpage";
-import Contactpage from "./pages/Contactpage";
-import Solutionspage from "./pages/Solutionspage";
-import Blogspage from "./pages/Blogspage";
-import Careerspage from "./pages/Careerspage";
+import Homepage from "./pages/Homepage.jsx";
+import AboutPage from "./pages/Aboutpage.jsx";
+import Contactpage from "./pages/Contactpage.jsx";
+import Solutionspage from "./pages/Solutionspage.jsx";
+import Blogspage from "./pages/Blogspage.jsx";
+import Careerspage from "./pages/Careerspage.jsx";
 import RootLayout from "./layouts/RootLayout";
 function App() {
   return (
