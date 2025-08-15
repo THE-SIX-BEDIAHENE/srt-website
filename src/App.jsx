1d885e1b046057c724aa439aff866b7df744fbe4
Mckay-z <waltermacquan@gmail.com>
2025-08-15 20:05:53 +0000

    updated path

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import HomePage from "./pages/HomePage.jsx";
-import ContactPage from "./pages/ContactPage.jsx";
 import SolutionsPage from "./pages/SolutionsPage.jsx";
-import BlogsPage from "./pages/BlogsPage.jsx";
-import CareersPage from "./pages/CareersPage.jsx";
 import RootLayout from "./layouts/RootLayout";
 import Aboutpage from "./pages/AboutPage.jsx";
+import Contactpage from "./pages/ContactPage.jsx";
+import Careerspage from "./pages/CareersPage.jsx";
+import Blogspage from "./pages/BlogsPage.jsx";
+import Homepage from "./pages/HomePage.jsx";
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
         <Route path="/" element={<RootLayout />}>
-          <Route path="/" element={<HomePage />} />
+          <Route path="/" element={<Homepage />} />
           <Route path="/about" element={<Aboutpage />} />
-          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/contact" element={<Contactpage />} />
           <Route path="/solutions" element={<SolutionsPage />} />
-          <Route path="/blogs" element={<BlogsPage />} />
-          <Route path="/careers" element={<CareersPage />} />
+          <Route path="/blogs" element={<Blogspage />} />
+          <Route path="/careers" element={<Careerspage />} />
           </Route>
         </Routes>
       </BrowserRouter>
