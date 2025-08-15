6cb2522586e4ee7786e630856817e6d779a3378b
Mckay-z <waltermacquan@gmail.com>
2025-08-15 20:02:07 +0000

    updated about path

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/HomePage.jsx";
-import AboutPage from "./pages/Aboutpage.jsx";
 import ContactPage from "./pages/ContactPage.jsx";
 import SolutionsPage from "./pages/SolutionsPage.jsx";
 import BlogsPage from "./pages/BlogsPage.jsx";
 import CareersPage from "./pages/CareersPage.jsx";
 import RootLayout from "./layouts/RootLayout";
+import Aboutpage from "./pages/AboutPage.jsx";
 function App() {
   return (
     <>
@@ -14,7 +14,7 @@ function App() {
         <Routes>
         <Route path="/" element={<RootLayout />}>
           <Route path="/" element={<HomePage />} />
-          <Route path="/about" element={<AboutPage />} />
+          <Route path="/about" element={<Aboutpage />} />
           <Route path="/contact" element={<ContactPage />} />
           <Route path="/solutions" element={<SolutionsPage />} />
           <Route path="/blogs" element={<BlogsPage />} />
